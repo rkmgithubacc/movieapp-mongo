f77884222f23d91e9b06951e1dc91693e3be84f2
RoshanKM <roshan.techacc88@gmail.com>
2021-12-18 15:01:04 +0530

    Setting up project repository

---
+++ src/main/java/com/rkmgithubacc/movieapp/MovieappMongoApplication.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.movieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieappMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieappMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkmgithubacc/movieapp/MovieappMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.movieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieappMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
